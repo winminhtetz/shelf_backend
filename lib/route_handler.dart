07bc0489275594cf02934c6c7cb2bc93d436c181
winminhtetz <mgwinnminnhtet111@gmail.com>
2024-05-20 15:52:04 +0630

    feat: add post request for openai

--- lib/route_handler.dart
+++ lib/route_handler.dart
@@ -1,8 +1,45 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:http/http.dart' as http;
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-// final router = Router()..get('/tip', _tipHandler);
+final router = Router()..get('/tip', _tipHandler);
 
-// Future<Response> _tipHandler (Request request) async {
-//
-// }
+final apiKey = Platform.environment['OPENAI_API_KEY'];
+
+Future<Response> _tipHandler(Request request) async {
+  final url = Uri.parse('https://api.openai.com/v1/chat/completions');
+  final headers = {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer $apiKey',
+  };
+  final data = {
+    'model': 'gpt-3.5-turbo',
+    'messages': [
+      {
+        'role': 'user',
+        'content': 'Give me a random tip about using Flutter and Dart. '
+            'Keep it to one sentence.',
+      }
+    ],
+    'temperature': 1.0,
+  };
+
+  final response = await http.post(
+    url,
+    headers: headers,
+    body: jsonEncode(data),
+  );
+
+  if (response.statusCode == 200) {
+    final responseBody = jsonDecode(response.body);
+    final messageContent = responseBody['choices'][0]['message']['content'];
+    return Response.ok(messageContent);
+  } else {
+    return Response.internalServerError(
+      body: 'OpenAI request failed: ${response.body}',
+    );
+  }
+}
