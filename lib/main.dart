a10ad724f9f287390399504bb709c3b3e2953a54
winminhtetz <mgwinnminnhtet111@gmail.com>
2024-05-21 13:50:10 +0630

    feat: move main file below lib

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'dart:io';
+
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart';
+import 'package:shelf_backend/middleware.dart';
+import 'package:shelf_backend/route_handler.dart';
+
+void main(List<String> args) async {
+  final ip = InternetAddress.anyIPv4;
+
+  final handler = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(rejectBadRequest())
+      .addHandler(router.call);
+
+  final port = int.parse(Platform.environment['PORT'] ?? '8080');
+  final server = await serve(handler, ip, port);
+  print('Server listening on port ${server.port}');
+}
