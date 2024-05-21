3ffce1f87d955359343be908a67c790cbcfaf6a1
winminhtetz <mgwinnminnhtet111@gmail.com>
2024-05-21 13:38:20 +0630

    refactor: refactor code

--- bin/server.dart
+++ bin/server.dart
@@ -8,12 +8,11 @@ import 'package:shelf_backend/route_handler.dart';
 void main(List<String> args) async {
   final ip = InternetAddress.anyIPv4;
 
-  final handler = Pipeline()
-      .addMiddleware(logRequests())
-      .addMiddleware(rejectBadRequest())
-      .addHandler(router.call);
+  final handler =
+      Pipeline().addMiddleware(logRequests()).addMiddleware(rejectBadRequest());
+  // .addHandler(router.call);
 
   final port = int.parse(Platform.environment['PORT'] ?? '8080');
-  final server = await serve(handler, ip, port);
-  print('Server listening on port ${server.port}');
+  // final server = await serve(handler, ip, port);
+  // print('Server listening on port ${server.port}');
 }
